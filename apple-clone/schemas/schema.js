e99085702c52ee70fbf5c1cbc4b8990ca014ddc2
NarkozC <sasiogludogucan@gmail.com>
2022-09-18 06:40:09 +0300

    build: implement vscode settings and launch.json debugger

--- apple-clone/schemas/schema.js
+++ apple-clone/schemas/schema.js
@@ -2,16 +2,16 @@
 import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
-import { user, account } from 'next-auth-sanity/schemas';
+import { account, user } from 'next-auth-sanity/schemas';
 
 // We import object and document schemas
 import blockContent from './blockContent';
 import category from './category';
 import product from './product';
 
+import localeBlockContent from './locale/BlockContent';
 import localeString from './locale/String';
 import localeText from './locale/Text';
-import localeBlockContent from './locale/BlockContent';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
