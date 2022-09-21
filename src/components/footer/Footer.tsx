054a0b990f1e6f3e2d00ce62eb1796e4075b9364
NarkozC <sasiogludogucan@gmail.com>
2022-09-21 10:33:56 +0300

    style: changed white-space on Footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import FooterSupport from './support/FooterSupport';
 
 const Footer = () => {
   return (
-    <div className="bg-promo-dark px-3 py-40">
+    <div className="bg-promo-dark px-3 py-20 sm:py-40">
       <div className="footer-text mx-auto grid max-w-xl grid-cols-1 grid-rows-1 gap-y-12 px-4 text-gray-400 sm:grid-cols-2 sm:grid-rows-2 md:max-w-7xl md:grid-cols-4 md:grid-rows-1">
         <FooterLogonSocials />
 
